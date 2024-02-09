9a427ef3c8027e70c94f1a8d41f35b2c9e7c1753
uliyana <uliyaiyaas@gmail.com>
2024-02-09 15:50:05 +0200

    Добавьте файлы проекта.

---
+++ lab3Chernyshenko/LongLong.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3Chernyshenko
+{
+    class LongLong
+    {
+        public long First { get; set; }
+        public ulong Second { get; set; }
+        public static LongLong operator +(LongLong f1, LongLong f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double res = Math.Round(a1 + a2, 3);
+            return new LongLong() { First = (long)Math.Floor(res), Second = ulong.Parse(res.ToString().Split(",")[1]) };
+        }
+        public static LongLong operator -(LongLong f1, LongLong f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double res = Math.Round(a1 - a2, 3);
+            return new LongLong() { First = (long)Math.Floor(res), Second = ulong.Parse(res.ToString().Split(',')[1]) };
+        }
+        public static LongLong operator *(LongLong f1, LongLong f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double res = Math.Round(a1 * a2, 3);
+            return new LongLong() { First = (long)Math.Floor(res), Second = ulong.Parse(res.ToString().Split(",")[1]) };
+        }
+        public static LongLong operator /(LongLong f1, LongLong f2)
+        {
+            double a1 = f1.First + (f1.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double a2 = f2.First + (f2.Second / Math.Pow(10, f1.Second.ToString().Length));
+            double res = Math.Round(a1 / a2, 3);
+            return new LongLong() { First = (long)Math.Floor(res), Second = ulong.Parse(res.ToString().Split(",")[1]) };
+        }
+        public static bool operator ==(LongLong f1, LongLong f2)
+        {
+            if (f1.First == f2.First && f1.Second == f2.Second)
+                return true;
+            else return false;
+        }
+        public static bool operator !=(LongLong f1, LongLong f2)
+        {
+            if (f1.First != f2.First || f1.Second != f2.Second)
+                return true;
+            else return false;
+        }
+
+        public override string? ToString()
+        {
+            return First + "," + Second;
+        }
+    }
+}
---
+++ lab3Chernyshenko/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3Chernyshenko
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LongLong f1 = new LongLong()
+            {
+                First = long.Parse(FirstOne.Text),
+                Second = ulong.Parse(SecondOne.Text)
+            };
+            LongLong f2 = new LongLong()
+            {
+                First = long.Parse(FirstTwo.Text),
+                Second = ulong.Parse(SecondTwo.Text)
+            };
+            LongLong res = f1 + f2;
+            Result.Text = res.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LongLong f1 = new LongLong()
+            {
+                First = long.Parse(FirstOne.Text),
+                Second = ulong.Parse(SecondOne.Text)
+            };
+            LongLong f2 = new LongLong()
+            {
+                First = long.Parse(FirstTwo.Text),
+                Second = ulong.Parse(SecondTwo.Text)
+            };
+            LongLong res = f1 - f2;
+            Result.Text = res.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            LongLong f1 = new LongLong()
+            {
+                First = long.Parse(FirstOne.Text),
+                Second = ulong.Parse(SecondOne.Text)
+            };
+            LongLong f2 = new LongLong()
+            {
+                First = long.Parse(FirstTwo.Text),
+                Second = ulong.Parse(SecondTwo.Text)
+            };
+            LongLong res = f1 * f2;
+            Result.Text = res.ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            LongLong f1 = new LongLong()
+            {
+                First = long.Parse(FirstOne.Text),
+                Second = ulong.Parse(SecondOne.Text)
+            };
+            LongLong f2 = new LongLong()
+            {
+                First = long.Parse(FirstTwo.Text),
+                Second = ulong.Parse(SecondTwo.Text)
+            };
+            Result.Text = (f1 == f2).ToString();
+        }
+    }
+}
